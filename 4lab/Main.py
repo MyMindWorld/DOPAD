a2ff72785ff7f4e76a92bade844876b084337d05
MyMindWorld <MyMindWorld@users.noreply.github.com>
2018-12-10 00:53:54 +0300

    -lab

--- 4lab/Main.py
+++ 4lab/Main.py
@@ -1,5 +1,4 @@
 import re
-from operator import itemgetter
 from natsort import natsorted
 def sort_and_format():
     uts_raw = []
@@ -29,12 +28,47 @@ def sort_and_format():
     #print('\n'.join(uts))
     w = open("UserToServer.txt", "w")
     w.write('\n'.join(uts))
-def find_mistakes():
+def del_garbage():
     w = open("STU.txt", "w")
     with open("ServerToUser.txt", "r+") as f:
         for line in f:
             line = re.sub('[ ]', '', line)
             line1 = line.split(";")[1]
-            print(line1)
             w.write(''.join(line1)) # Каждое решение сервера в отдельную строку без лишних данных
-find_mistakes()
+    w = open("UTS.txt", "w")
+    c = 0
+    with open("UserToServer.txt", "r+") as f:
+        for line in f:
+            c +=1
+            line = re.sub('[ ]', '', line)
+            line1 = line.split(",")[2]
+            if c < 7:
+                line1 = line1.replace("\n", '')
+            else:
+                line1 = line1 + '\n'
+                c = 0
+            w.write(''.join(line1))  # Каждое решение сервера в отдельную строку без лишних данных
+
+def compare():
+    c = 0
+    w = open("UTS_answers.txt", "w")
+    with open("UTS.txt", "r+") as f:
+        for line in f:
+            c +=1
+            if line == '\n':
+                error =+ 1
+            else:
+                i = eval(str(line))
+                w.write(str(i))
+                if c != 79:
+                    w.write('\n')
+    w.close()
+    f.close()
+    with open("UTS_answers.txt", "r+") as f1, \
+         open("STU.txt", "r+") as f2:
+            for real, server in zip(f1, f2):  # читать построчно оба файла
+                c += 1
+                if real != server:
+                    print('В выражении №', c, ' ошибка', real, ' не равно', server)
+
+compare()
