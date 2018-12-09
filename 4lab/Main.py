593944da891fd497602fab250c65478d23438912
MyMindWorld <MyMindWorld@users.noreply.github.com>
2018-12-09 19:40:42 +0300

    Убрал мусор из stu

--- 4lab/Main.py
+++ 4lab/Main.py
@@ -14,11 +14,12 @@ def sort_and_format():
                 uts_raw.append(line)
     for line in stu_raw:
         str(line)
-        line = re.sub('["= ]', '', line)
+        line = re.sub('["=]', '', line)
         line = re.sub('[,]', ';', line)
         stu.append(re.compile("\((.*)\)").search(line).group(1))
     for line in uts_raw:
         str(line)
+        line = re.sub('["]', '', line)
         uts.append(re.compile("\((.*)\)").search(line).group(1))
     stu = natsorted(stu)
     #print('\n'.join(stu))
@@ -28,16 +29,12 @@ def sort_and_format():
     #print('\n'.join(uts))
     w = open("UserToServer.txt", "w")
     w.write('\n'.join(uts))
-'''    w = open("out.txt", "w")
-    for i in stu:
-        w.write(''.join(i)+'\n')'''
-'''with open("IKB_3_6.txt") as file: 
-    for line in file:
-        str(line)
-        d = re.compile( "\((.*)\)" ).search( line ).group( 1 )
-        w = open("out.txt","w")
-        w.write("".join(d))
-'''
-
-
-sort_and_format()
+def find_mistakes():
+    w = open("STU.txt", "w")
+    with open("ServerToUser.txt", "r+") as f:
+        for line in f:
+            line = re.sub('[ ]', '', line)
+            line1 = line.split(";")[1]
+            print(line1)
+            w.write(''.join(line1)) # Каждое решение сервера в отдельную строку без лишних данных
+find_mistakes()
